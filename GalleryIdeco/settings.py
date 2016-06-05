bdf59c16d6672ded64dfc4d6b1451fdff5998ee2
Savi <Ultimate95@mail.ru>
2016-06-05 16:21:40 +0500

    some updates for deploy

--- GalleryIdeco/settings.py
+++ GalleryIdeco/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@zx7l9p=%i)c32tka@jy9vcja6u15p%(cn1$fk1_s+r1l8&l3k'  # NOQA
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = THUMBNAIL_DEBUG = True
+DEBUG = THUMBNAIL_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,8 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
 
 from gallery.views import gallery_page, get_images
 
 urlpatterns = [
     url(r'^$', gallery_page),
     url(r'^get-images/(?P<offset>\d+)/$', get_images)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
