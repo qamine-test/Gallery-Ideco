a4983605c5c165af8a0bf988ab7838b5fee1da25
Savi <Ultimate95@mail.ru>
2016-06-05 17:01:00 +0500

    some updates for deploy

--- GalleryIdeco/urls.py
+++ GalleryIdeco/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('gallery.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + patterns('',
+        (
+            r'^static/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}
+        ),
+    )
